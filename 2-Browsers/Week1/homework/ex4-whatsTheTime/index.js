7542c3f9fd789ca54f9f3708f5f18ccf89c7e2c6
yusufelik <ysfelik1@gmail.com>
2022-11-22 19:34:18 +0100

    ex4 done

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -8,7 +8,44 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
-  // TODO complete this function
+  const newDiv = document.createElement('div');
+  const newP = document.createElement('p');
+  newP.textContent = 'loading...';
+  //Time formatting solution from https://stackoverflow.com/questions/18229022/how-to-show-current-time-in-javascript-in-the-format-hhmmss
+
+  function checkTime(i) {
+    //adds zero if parameter<10
+    return i < 10 ? '0' + i : i;
+  }
+  //gets hour minute and second of current date and sets text of 'p' element
+  function startTime() {
+    const today = new Date(),
+      h = checkTime(today.getHours()),
+      m = checkTime(today.getMinutes()),
+      s = checkTime(today.getSeconds());
+    newP.textContent = h + ':' + m + ':' + s;
+    //works every second to get realtime
+  }
+
+  newP.style.fontSize = '2rem';
+  newP.style.margin = '0';
+
+  newDiv.style.border = ' 5px solid';
+  newDiv.style.textAlign = 'center';
+  newDiv.style.width = '40vw';
+  newDiv.style.height = '35px';
+  //centering div
+  newDiv.style.margin = 'auto';
+  newDiv.style.top = '0';
+  newDiv.style.left = '0';
+  newDiv.style.right = '0';
+  newDiv.style.bottom = '0';
+  newDiv.style.position = 'absolute';
+
+  newDiv.appendChild(newP);
+  document.body.appendChild(newDiv);
+
+  window.setInterval(startTime, 1000);
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+window.addEventListener('load', addCurrentTime);
