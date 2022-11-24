1ca5c9c42afa83783cf95f3a20711fab430ecdc8
yusufelik <ysfelik1@gmail.com>
2022-11-24 23:59:51 +0100

    ex4 corrected and tested

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -7,45 +7,43 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
   second). Use `setInterval()` to make sure the time stays current.
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
-function addCurrentTime() {
-  const divContent = document.createElement('div');
-  const pTime = document.createElement('p');
-  pTime.textContent = 'loading...';
-  //Time formatting solution from https://stackoverflow.com/questions/18229022/how-to-show-current-time-in-javascript-in-the-format-hhmmss
 
+function addCurrentTime() {
+  //gets hour minute and second of current date and sets text of 'p' element
+  const today = new Date(),
+    h = checkTime(today.getHours()),
+    m = checkTime(today.getMinutes()),
+    s = checkTime(today.getSeconds());
+  pTime.textContent = h + ':' + m + ':' + s;
+  console.log(pTime.textContent);
+
+  //Time formatting solution from https://stackoverflow.com/questions/18229022/how-to-show-current-time-in-javascript-in-the-format-hhmmss
   function checkTime(i) {
     //adds zero if parameter<10
     return i < 10 ? '0' + i : i;
   }
-  //gets hour minute and second of current date and sets text of 'p' element
-  function startTime() {
-    const today = new Date(),
-      h = checkTime(today.getHours()),
-      m = checkTime(today.getMinutes()),
-      s = checkTime(today.getSeconds());
-    pTime.textContent = h + ':' + m + ':' + s;
-    console.log(pTime.textContent);
-  }
-
-  pTime.style.fontSize = '2rem';
-  pTime.style.margin = '0';
-
-  divContent.style.border = ' 5px solid';
-  divContent.style.textAlign = 'center';
-  divContent.style.width = '40vw';
-  divContent.style.height = '35px';
-  //centering div
-  divContent.style.margin = 'auto';
-  divContent.style.top = '0';
-  divContent.style.left = '0';
-  divContent.style.right = '0';
-  divContent.style.bottom = '0';
-  divContent.style.position = 'absolute';
-
-  divContent.appendChild(pTime);
-  document.body.appendChild(divContent);
-  //works every second to get realtime
-  window.setInterval(startTime, 1000);
 }
+const divContent = document.createElement('div');
+const pTime = document.createElement('p');
+pTime.textContent = 'loading...';
+divContent.appendChild(pTime);
+document.body.appendChild(divContent);
 
-window.addEventListener('load', addCurrentTime);
+pTime.style.fontSize = '2rem';
+pTime.style.margin = '0';
+
+divContent.style.border = ' 5px solid';
+divContent.style.textAlign = 'center';
+divContent.style.width = '40vw';
+divContent.style.height = '35px';
+//centering div
+divContent.style.margin = 'auto';
+divContent.style.top = '0';
+divContent.style.left = '0';
+divContent.style.right = '0';
+divContent.style.bottom = '0';
+divContent.style.position = 'absolute';
+
+//works every second to get realtime
+const myInterval = setInterval(addCurrentTime, 1000);
+window.onload = myInterval;
