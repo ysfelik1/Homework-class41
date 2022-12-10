b43c9134da029eaa421c09ea1c2f4728c74499b0
yusufelik <ysfelik1@gmail.com>
2022-12-10 23:50:05 +0100

    ex3 done

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -11,48 +11,45 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-Usin
   explanation? Add your answer as a comment to be bottom of the file.
 ------------------------------------------------------------------------------*/
 
-// TODO Remove callback and return a promise
-function rollDie(callback) {
+function rollDie() {
   // Compute a random number of rolls (3-10) that the die MUST complete
   const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
   console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
 
-  const rollOnce = (roll) => {
-    // Compute a random die value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die value is now: ${value}`);
+  const myPromise = new Promise(function (resolve, reject) {
+    const rollOnce = (roll) => {
+      // Compute a random die value for the current roll
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Die value is now: ${value}`);
 
-    // Use callback to notify that the die rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Die rolled off the table.'));
-    }
+      // Use callback to notify that the die rolled off the table after 6 rolls
+      if (roll > 6) {
+        reject(new Error('Oops... Die rolled off the table.'));
+      }
+      // Use callback to communicate the final die value once finished rolling
+      if (roll === randomRollsToDo) {
+        resolve(value);
+      }
+      // Schedule the next roll todo until no more rolls to do
+      if (roll < randomRollsToDo) {
+        setTimeout(() => rollOnce(roll + 1), 500);
+      }
+    };
 
-    // Use callback to communicate the final die value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
-
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
-
-  // Start the initial roll
-  rollOnce(1);
+    // Start the initial roll
+    rollOnce(1);
+  });
+  return myPromise;
 }
 
 function main() {
-  // TODO Refactor to use promise
-  rollDie((error, value) => {
-    if (error !== null) {
-      console.log(error.message);
-    } else {
+  rollDie()
+    .then(function (value) {
       console.log(`Success! Die settled on ${value}.`);
-    }
-  });
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
 }
 
 // ! Do not change or remove the code below
