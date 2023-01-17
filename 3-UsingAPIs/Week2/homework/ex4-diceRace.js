ddc4a82128e6e825c4854ca3609f9359d9b2a33d
yusufelik <ysfelik1@gmail.com>
2023-01-17 22:44:00 +0100

    ex4 done

--- 3-UsingAPIs/Week2/homework/ex4-diceRace.js
+++ 3-UsingAPIs/Week2/homework/ex4-diceRace.js
@@ -15,14 +15,18 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
   const dice = [1, 2, 3, 4, 5];
-  // TODO complete this function; use Promise.race() and rollDie()
+  const promises = dice.map((die) => rollDie(die));
+  return Promise.race(promises);
 }
 
 // Refactor this function to use async/await and try/catch
-function main() {
-  rollDice()
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const results = await rollDice();
+    console.log(results);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 // ! Do not change or remove the code below
@@ -30,3 +34,5 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDice;
+
+//My answer; Because the Promise.race() method brings the fastest ones result among the promises. Does not wait for other promises to finish.
